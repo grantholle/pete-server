2d56e3aeeda635a87c8cbdb1035c7a12f2506f55
Grant Holle <hollegrant@gmail.com>
2019-05-20 21:39:46 +0800

    Add shows migration

---
+++ database/migrations/1558358865558_shows_schema.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class ShowsSchema extends Schema {
+  up () {
+    this.create('shows', (table) => {
+      table.increments()
+      table.bigInteger('tmdb_id').notNullable()
+      table.string('imdb_id').nullable()
+      table.string('name').notNullable()
+      table.integer('start_season').defaultTo(1)
+      table.string('quality').defaultTo('HDTV')
+      table.boolean('use_alt_quality').defaultTo(true)
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('shows')
+  }
+}
+
+module.exports = ShowsSchema
