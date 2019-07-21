b61deb195a51525b58a6234e00f8513ee556ee61
Grant Holle <hollegrant@gmail.com>
2019-07-21 17:11:40 +0800

    Refactor the tv watchlist component to fetch on mount

--- resources/js/components/TvWatchlist.svelte
+++ resources/js/components/TvWatchlist.svelte
@@ -2,21 +2,26 @@
   import axios from 'axios'
   import { onMount } from 'svelte'
 
-  let results = axios.get('/api/v1/tv')
+  let shows = []
+
+  onMount(async () => {
+    const { data } = await axios.get('/api/v1/tv')
+    shows = data
+  })
 </script>
 
 <div class="container">
 <h2>TV Watchlist</h2>
 
-{#await results}
+{#if (shows.length === 0)}
   <p class="text-center">Loading watchlist...</p>
-{:then data}
+{:else}
   <div class="flex flex-wrap -mx-4">
-    {#each data.data as show}
+    {#each shows as show}
       <div class="w-1/2 sm:w-1/3 lg:w-1/4 px-4 mb-8">
         <img class="block w-full" src="//image.tmdb.org/t/p/w342{show.poster_path}" alt={show.name} />
       </div>
     {/each}
   </div>
-{/await}
+{/if}
 </div>
