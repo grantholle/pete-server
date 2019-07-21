5f2a66f0da8b2d45cee95c87fd0c12fe076c3a9a
Grant Holle <hollegrant@gmail.com>
2019-07-21 17:12:02 +0800

    Fix indentation

--- resources/js/components/TvWatchlist.svelte
+++ resources/js/components/TvWatchlist.svelte
@@ -11,17 +11,17 @@
 </script>
 
 <div class="container">
-<h2>TV Watchlist</h2>
+  <h2>TV Watchlist</h2>
 
-{#if (shows.length === 0)}
-  <p class="text-center">Loading watchlist...</p>
-{:else}
-  <div class="flex flex-wrap -mx-4">
-    {#each shows as show}
-      <div class="w-1/2 sm:w-1/3 lg:w-1/4 px-4 mb-8">
-        <img class="block w-full" src="//image.tmdb.org/t/p/w342{show.poster_path}" alt={show.name} />
-      </div>
-    {/each}
-  </div>
-{/if}
+  {#if (shows.length === 0)}
+    <p class="text-center">Loading watchlist...</p>
+  {:else}
+    <div class="flex flex-wrap -mx-4">
+      {#each shows as show}
+        <div class="w-1/2 sm:w-1/3 lg:w-1/4 px-4 mb-8">
+          <img class="block w-full" src="//image.tmdb.org/t/p/w342{show.poster_path}" alt={show.name} />
+        </div>
+      {/each}
+    </div>
+  {/if}
 </div>
